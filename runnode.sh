405ea1ff152f74df2dec324fc0f9b1e6524a80cd
Václav Pavlín <vaclav.pavlin@gmail.com>
2023-04-14 17:36:13 +0200

    Use replication for scaling to test dynamic IP assignment

---
+++ runnode.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+IP=$(ip a | grep "inet " | grep -Fv 127.0.0.1 | sed 's/.*inet \([^/]*\).*/\1/')
+echo $IP
+echo ${BOOTSTRAP_ENR}
+
+ exec /usr/bin/wakunode\
+      --relay=true\
+      --rpc-admin=true\
+      --keep-alive=true\
+      --max-connections=150\
+      --dns-discovery=true\
+      --discv5-discovery=true\
+      --discv5-enr-auto-update=True\
+      --log-level=INFO\
+      --rpc-address=0.0.0.0\
+      --metrics-server=True\
+      --metrics-server-address=0.0.0.0\
+      --discv5-bootstrap-node=${BOOTSTRAP_ENR}\
+      --nat=extip:${IP}

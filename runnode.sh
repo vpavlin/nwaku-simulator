e985d14bad05d661699e462b94cd9829d3b16c45
Václav Pavlín <vaclav.pavlin@gmail.com>
2023-04-21 11:22:36 +0200

    Add WSS support

--- runnode.sh
+++ runnode.sh
@@ -17,7 +17,7 @@ if [ -n "${TRAFFIC_GENERATOR}" ]; then
   exec /main\
       --pubsub-topic="my-ptopic"\
       --content-topic="my-ctopic"\
-      --msg-per-second=100\
+      --msg-per-second=50\
       --msg-size-kb=2\
       --bootstrap-node=${NODE_ENR}
 fi
@@ -58,18 +58,47 @@ else
 
   echo "Using bootstrap node: ${BOOTSTRAP_ENR}"
 
-  exec /usr/bin/wakunode\
+  exec usr/bin/wakunode\
         --relay=true\
+        --store=true\
+        --filter=true\
+        --lightpush=true\
+        --rln-relay=true\
+        --peer-exchange=true\
         --rpc-admin=true\
+        --peer-persistence=false\
         --keep-alive=true\
         --max-connections=150\
-        --dns-discovery=true\
+        --topics="/waku/2/default-waku/proto /waku/2/dev-waku/proto"\
+        --store-message-db-url=sqlite:///data/store.sqlite3\
+        --store-message-retention-policy=time:2592000\
+        --rln-relay-content-topic=/toy-chat/2/luzhou/proto\
+        --rln-relay-dynamic=True\
+        --rln-relay-eth-account-address=0000000000000000000000000000000000000000\
+        --rln-relay-eth-contract-address=0x4252105670fE33D2947e8EaD304969849E64f2a6\
+        --rln-relay-eth-client-address=wss://goerli.infura.io/ws/v3/6af8fe9ec359410f9d6d26cbf746b86c\
+        --websocket-port=8000\
+        --websocket-secure-support=true\
+        --websocket-secure-key-path=/opt/waku/key.pem\
+        --websocket-secure-cert-path=/opt/waku/cert.pem\
         --discv5-discovery=true\
-        --discv5-enr-auto-update=True\
-        --log-level=INFO\
-        --rpc-address=0.0.0.0\
-        --metrics-server=True\
-        --metrics-server-address=0.0.0.0\
+        --discv5-udp-port=9000\
+        --discv5-enr-auto-update=False\
         --discv5-bootstrap-node=${BOOTSTRAP_ENR}\
-        --nat=extip:${IP}
+        --nat=extip:${IP}\
+        --log-level=DEBUG\
+        --rpc-port=8545\
+        --rpc-address=0.0.0.0\
+        --tcp-port=30303\
+        --metrics-server=True\
+        --metrics-server-address=0.0.0.0
+ 
+
+#        --dns-discovery=true\
+#        --dns-discovery-url=enrtree://AOGECG2SPND25EEFMAJ5WF3KSGJNSGV356DSTL2YVLLZWIV6SAYBM@test.waku.nodes.status.im\
+#        --nodekey=06873027778e85482aaf152c41b4bdef6a7cbae345052ac9a986e29fffacd7b6\
+#        --dns4-domain-name="node-01.ac-cn-hongkong-c.wakuv2.test.statusim.net"\
+
+
+
 fi
